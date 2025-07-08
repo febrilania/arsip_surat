c0b59f8b1ab7991973766362d52601e914668936
febrilania <febrilanianurislami@gmail.com>
2025-07-08 09:57:18 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,7 +22,7 @@
 </x-app-layout> --}}
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard Admin</h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard {{Auth::user()->role}}</h2>
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="py-6 px-4 grid grid-cols-1 md:grid-cols-2 gap-6">
